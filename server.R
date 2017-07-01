87bf714e4155d61447395c5aa989627e6eca7953
chrismayer87 <chris.mayer87@gmail.com>
2017-07-01 13:50:20 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,17 @@
+
+
+# Server logic
+server <- function(input, output) {
+  
+  # change plot based on restrictions from input
+  output$Plot <- renderPlot({
+    plot(price ~ carat, diamonds[which(diamonds$cut == input$cutInput
+                                       & diamonds$color == input$colorInput
+                                       & diamonds$clarity == input$clarInput),])
+    # also put the linear model on the plot
+    abline(lm(price ~ carat, 
+              diamonds[which(diamonds$cut == input$cutInput
+                                       & diamonds$color == input$colorInput
+                                       & diamonds$clarity == input$clarInput),]))
+  })
+}
